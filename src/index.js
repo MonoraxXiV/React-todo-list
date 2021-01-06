a44b2aac712876ff9451ce28376b4eb9804def5f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-01-06 16:49:15 +0100

    finally displaying again

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./style.css"
 import TodoList from "./todolist";
 
+var destination = document.querySelector("#container")
+
+ReactDOM.render(
+    <div>
+        <TodoList/>
+    </div>,
+    destination
+);
--- src/todolist.js
+++ src/todolist.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 class TodoList extends Component {
     render() {
         return (
-            <div>
-                <form>
-                    <input placeholder="enter task">
-                    </input>
-                    <button type="submit">add</button>
-                </form>
+            <div className="todoListMain">
+                <div className="header">
+                    <form>
+                        <input placeholder="enter task">
+                        </input>
+                        <button type="submit">add</button>
+                    </form>
+                </div>
             </div>
         )
     }
